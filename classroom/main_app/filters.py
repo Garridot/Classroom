b715de3df8762952c160a721fc225b4dc989cbe8
Garridot <garridot210@gmail.com>
2021-09-11 12:49:14 -0300

    filters.py created

---
+++ classroom/main_app/filters.py
@@ -0,0 +1,6 @@
+from django.contrib.auth.models import User
+from django.forms.widgets import TextInput,SelectMultiple 
+from django import forms
+from django_filters import DateFilter
+import django_filters
+from .models import *
