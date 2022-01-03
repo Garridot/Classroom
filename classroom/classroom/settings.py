e1c9106aebb7bd89377687d0220c6d878aeadcdd
Garridot <garridot210@gmail.com>
2022-01-03 17:51:09 -0300

    STATIC_ROOT

--- classroom/classroom/settings.py
+++ classroom/classroom/settings.py
@@ -133,7 +133,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 import os
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS=[os.path.join(BASE_DIR,'static')]
 STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
