d4d998e52b053e245692c20e1143fbc5b8bd2b73
Garridot <garridot210@gmail.com>
2022-01-03 16:31:57 -0300

    a

--- classroom/classroom/settings.py
+++ classroom/classroom/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-a3^747$+rgtofl7f(#+hgr-99qbntl0di=@orc0bo)#5#zr8^a
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://web-academy-heroku.herokuapp.com/', 'http://127.0.0.1:8000/']
 
 
 # Application definition
